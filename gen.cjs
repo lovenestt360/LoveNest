6b1b45fb5b0faca7cc72e24841d94a2e7c85c892
Dilson <Dilson.Quissico@gmail.com>
2026-03-22 23:36:55 -0300

    chore: major project cleanup and optimization. removed unused heavy dependencies and redundant backup files.

--- gen.cjs
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-const ecdh = crypto.createECDH('prime256v1');
-ecdh.generateKeys();
-const publicKey = ecdh.getPublicKey('base64', 'uncompressed');
-const privateKey = ecdh.getPrivateKey('base64');
-console.log(JSON.stringify({ publicKey, privateKey }));
